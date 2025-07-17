99d500137d1c500702b34dca9ca6239badd85130
Pedro Souza <pedro.olegario23@gmail.com>
2025-07-16 22:20:16 -0300

    feat: suspense API no react

--- src/app/catalog/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default async function CatalogLoading() {
-    return <h1>Carregando Catálogo...</h1>
-} 
--- src/app/catalog/product/[...data]/test.tsx
+++
@@ -1,6 +0,0 @@
-// server component to client component
-export async function Test() {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
-  return <h1>Hello World</h1>
-}
